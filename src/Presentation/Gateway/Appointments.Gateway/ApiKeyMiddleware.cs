58c22c147781c16d7e0024c4c35e25274ff178be
jeronimoguevararogel <guevararogeljj@icloud.com>
2025-08-05 15:33:29 -0600

    add jwt config and apikey to call API

--- src/Presentation/Gateway/Appointments.Gateway/ApiKeyMiddleware.cs
+++ src/Presentation/Gateway/Appointments.Gateway/ApiKeyMiddleware.cs
@@ -1,29 +1,48 @@
 namespace Appointments.Gateway;
 
-public class ApiKeyMiddleware {
+public class ApiKeyMiddleware
+{
     private readonly RequestDelegate _next;
-    private
-        const string APIKEY = "XApiKey";
-    public ApiKeyMiddleware(RequestDelegate next) {
+    private const string APIKEY = "XApiKey";
+
+    public ApiKeyMiddleware(RequestDelegate next)
+    {
         _next = next;
     }
-    public async Task InvokeAsync(HttpContext context) {
-        if (!context.Request.Headers.TryGetValue(APIKEY, out
-                var extractedApiKey)) {
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
+        {
             context.Response.StatusCode = 401;
             await context.Response.WriteAsync("Api Key was not provided ");
             return;
         }
 
-        context.Request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImRmN2I5MGY0LWI5MzEtNGFmOS1iZTk5LTE0MThjNjg2NDhhZiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImFkbWluQGxvY2FsaG9zdC5jb20iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbmlzdHJhdG9yIiwiZXhwIjoxNzUxNTY1OTU5LCJpc3MiOiJBcHBvaW50bWVudHNBUEkiLCJhdWQiOiJBcHBvaW50bWVudHNDbGllbnQifQ.4Jr5_e5ROpj6c2rrw3W9-qWfNEtsU9VMAai3R6omn7o");
-        
-        var appSettings = context.RequestServices.GetRequiredService < IConfiguration > ();
-        var apiKey = appSettings.GetValue < string > (APIKEY);
-        if (!apiKey.Equals(extractedApiKey)) {
+        // Reemplaza o agrega el encabezado Authorization
+        //context.Request.Headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjY3NjljYzdiLWE4NjYtNDI4NC1hMWQ3LTMwNGFiNjI3MjQwNyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImNsaWVudEBsb2NhbGhvc3QuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImNsaWVudCIsImlkVXNlciI6IjY3NjljYzdiLWE4NjYtNDI4NC1hMWQ3LTMwNGFiNjI3MjQwNyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluaXN0cmF0b3IiLCJleHAiOjE3NTQ0MzIyNDIsImlzcyI6IkFwcG9pbnRtZW50c0FQSSIsImF1ZCI6IkFwcG9pbnRtZW50c0NsaWVudCJ9.I2UGfHNUh_glLdPS6i5jHRC90ySdDwln7ktnoDKnnOQ";
+
+        //get header Bearer token
+        var bearerToken = context.Request.Headers["Bearer"].ToString();
+        //add to Authorization header
+        if (!string.IsNullOrEmpty(bearerToken))
+        {
+            context.Request.Headers["Authorization"] = $"Bearer {bearerToken}";
+        }
+        else
+        {
+            await context.Response.WriteAsync("Bearer token was not provided ");
+        }
+
+        var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+        var apiKey = appSettings.GetValue<string>(APIKEY);
+        if (!apiKey.Equals(extractedApiKey))
+        {
             context.Response.StatusCode = 401;
             await context.Response.WriteAsync("Unauthorized client");
             return;
         }
+
         await _next(context);
     }
 }
--- src/Presentation/Gateway/Appointments.Gateway/Program.cs
+++ src/Presentation/Gateway/Appointments.Gateway/Program.cs
@@ -1,6 +1,10 @@
+using System.Text;
 using Appointments.Gateway;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
@@ -13,11 +17,33 @@ builder.Services.AddCors(options =>
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+// Configuración de JWT antes de Build()
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
 var app = builder.Build();
-//Eneable cors
+
 app.UseCors("AllowAll");
+app.UseMiddleware<ApiKeyMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapReverseProxy();
 app.MapGet("/", () => "Hello World! yarp");
-app.UseMiddleware<ApiKeyMiddleware>();
 
 app.Run();
